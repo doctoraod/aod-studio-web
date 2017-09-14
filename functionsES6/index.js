f75044fc096ccaccb759f69c1dc2a297cdd26b3e
Suvichan Prasongphonchai <acspdoctoraod@gmail.com>
2017-09-14 17:00:33 +0700

    Add function

--- functionsES6/index.js
+++ functionsES6/index.js
@@ -1,8 +1,5 @@
-const functions = require('firebase-functions');
+import * as functions from 'firebase-functions'
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.check = functions.https.onRequest((request, response) => {
+  response.json({ status: true })
+})
